9cd08339695f4eb11ecd4de1c54deb73ab660532
hudchant <hudchant@gmail.com>
2026-01-30 20:27:57 -0800

    Add function for running a single math operation

--- script.js
+++ script.js
@@ -24,20 +24,16 @@ function divide(a, b) {
     return quotient;
 }
 
-let firstNum; // First number in math equation
-let secondNum; // Second number in math equation
-let operator; // Operator in math equation
-
 // Logic for performing math equations
 function operate(firstNum, secondNum, operator) {
     switch (operator) {
-        case "+":
+        case '+':
             return add(firstNum, secondNum);
-        case "-":
+        case '-':
             return subtract(firstNum, secondNum);
-        case "*":
+        case '×':
             return multiply(firstNum, secondNum);
-        case "/":
+        case '÷':
             return divide(firstNum, secondNum);
         default:
             "Invalid selection. Please try again."
@@ -45,4 +41,54 @@ function operate(firstNum, secondNum, operator) {
 }
 
 
+// Listen for button clicks
+document.addEventListener('DOMContentLoaded', function () {
+
+    let firstNum = undefined; // First number in math equation
+    let secondNum = undefined; // Second number in math equation
+    let operator = undefined; // Operator in math equation
+    let userInput = ''; // Empty input to store user selections when buttons are pressed
+    let numberSelection = document.querySelector('.numbers'); // Reference to the HTML number display div
+
+    const allButtons = document.querySelectorAll('button');
+    allButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const selection = button.id; // User selection is linked to the id of the button pressed
+
+            // If user selects number(s), store them in the userInput variable and push them onto the display
+            if (!isNaN(selection)) {
+                userInput += selection;
+                numberSelection.textContent = userInput;
+
+                // If user selects an operator, store previous number(s) in firstNum variable,
+                // set operator as the current selection, and clear userInput in preparation to receive secondNum
+            } else if ((selection === '+' || selection === '-' || selection === '×' || selection === '÷') && userInput) {
+                firstNum = parseFloat(userInput);
+                operator = selection;
+                userInput = '';
+
+                // If equal sign is pressed and firstNum and operator are already set, store next number(s)
+                // in secondNum variable and call operate() function on the operation
+            } else if (selection === '=' && firstNum !== null && operator && userInput) {
+                secondNum = parseFloat(userInput);
+                let answer = operate(firstNum, secondNum, operator);
+                numberSelection.textContent = answer; // Display answer on screen
+
+                // Reset all fields except firstNum, which will hold the previous answer until next operation called on it
+                firstNum = answer;
+                secondNum = undefined;
+                operator = undefined;
+                userInput = '';
+
+                // If 'C' (clear) button is pressed, reset all fields including the display
+            } else if (selection === 'C') {
+                firstNum = undefined;
+                secondNum = undefined;
+                operator = undefined;
+                userInput = '';
+                numberSelection.textContent = '';
+            }
+        });
+    });
+});
 
